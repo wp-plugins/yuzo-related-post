8b87239448416e95dab93c1adb0921a95f762076
iLenStudio <iLenStudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-02 09:49:24 +0000

    news changes
    git-svn-id: https://plugins.svn.wordpress.org/yuzo-related-post/trunk@1036586 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/ilenframework/core.php
+++ assets/ilenframework/core.php
@@ -463,7 +463,7 @@ class ilen_framework_1_7_3 {
 							<a class="ibtn btnred left btn_reset" data-me="<?php _e('Want to update all the default values​​ &#63;',$this->parameter['name_option']) ?>"><span><i class="fa fa-repeat"></i></span><?php _e('Reset',$this->parameter['name_option']) ?></a>
 							<?php if( isset($this->parameter['link_donate']) && $this->parameter['link_donate'] ): ?>
 							<a class="ibtn btngreen right btn_donate grow-btn" href="<?php echo $this->parameter['link_donate']; ?>" target="_blank" ><span style="width: 55px;"><i  class="fa fa-cc-paypal" style="font-size: 23px;margin-top: 6px;margin-left: 13px;" ></i></span><?php _e('Donate',$this->parameter['name_option']) ?></a>
-						<?php endif; ?>
+							<?php endif; ?>
 						</footer>
 						<script>
 						/*jQuery(document).ready(function($){
@@ -554,7 +554,7 @@ function ilentheme_options_wrap_for_plugin_tabs(){  ?>
 					<span class='ilen-version'><?php if( isset($this->parameter['method']) && $this->parameter['method'] == 'free' ){ echo  $this->parameter['version']. " " .__('',$this->parameter['name_option']); }else{ echo $this->parameter['version']. " " . __('',$this->parameter['name_option']); }  ?></span>
 				<?php endif; ?> 
 				</h2>
-				<?php if( isset($this->parameter['wp_review']) && $this->parameter['wp_review'] ): ?><a href="<?php echo $this->parameter['wp_review'] ?>" class="leave-a-review ibtn btnred right" target="_blank"><span><i class="fa fa-star"></i></span>Leave a review</a><?php endif; ?>
+				<?php if( isset($this->parameter['wp_review']) && $this->parameter['wp_review'] ): ?><a href="<?php echo $this->parameter['wp_review'] ?>" class="leave-a-review ibtn btnred right grow-btn" target="_blank"><span><i class="fa fa-star"></i></span>Leave a review</a><?php endif; ?>
 				<?php if( isset($this->parameter['twitter'] ) && $this->parameter['twitter'] ): ?><a href="<?php echo $this->parameter['twitter'] ?>" class="tweet-about-it ibtn btnturke right" target="_blank"><span><i class="fa fa-twitter"></i></span>Write your experience</a><?php endif; ?>
 				<?php if( isset($this->parameter['wp_support']) && $this->parameter['wp_support'] ): ?><a href="<?php echo $this->parameter['wp_support'] ?>" class="ibtn btngray2 right" target="_blank"><span><i class="fa fa-wrench"></i></span>Support</a><?php endif; ?>
 			</header>
@@ -675,6 +675,9 @@ function ilentheme_options_wrap_for_plugin_tabs(){  ?>
 				<?php if( !isset($only_page) || !$only_page ): ?>
 				<a href="#" class="ibtn btnblack left btn_save"><span><i class="fa fa-refresh"></i></span><?php _e('Save Changes',$this->parameter['name_option']) ?></a>
 				<a href="#" class="ibtn btnred left btn_reset" data-me="<?php _e('Want to update all the default values​​ &#63;',$this->parameter['name_option']) ?>"><span><i class="fa fa-repeat"></i></span><?php _e('Reset section',$this->parameter['name_option']) ?></a>
+				<?php if( isset($this->parameter['link_donate']) && $this->parameter['link_donate'] ): ?>
+				<a class="ibtn btngreen right btn_donate grow-btn" href="<?php echo $this->parameter['link_donate']; ?>" target="_blank" ><span style="width: 55px;"><i  class="fa fa-cc-paypal" style="font-size: 23px;margin-top: 6px;margin-left: 13px;" ></i></span><?php _e('Donate',$this->parameter['name_option']) ?></a>
+				<?php endif; ?>
 				<?php endif; ?>
 			</footer>
 			<script>
